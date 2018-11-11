a87b0661f86240f6e57f62021a3aa44ef29b6311
sorryisme <wivipp39@gmail.com>
2018-11-11 20:32:51 +0900

    작업일:2018. 11. 11 (일)
    -작업자:
    김지성(sorryisme)
    
    -작업 주제 :
    index 로그인 ajax 처리
    
    -내용:
    1.index 페이지 로그인 ajax처리
    2.login check 서블릿 생성 (ajax용)

---
+++ src/edu/shoppingMall/controller/userInfoController/SignCheckController.java
@@ -0,0 +1,32 @@
+package edu.shoppingMall.controller.userInfoController;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import edu.shoppingMall.dto.UserInfoDTO;
+
+@WebServlet("/signCheck")
+public class SignCheckController extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+		
+		PrintWriter out = response.getWriter();
+		UserInfoDTO dto = (UserInfoDTO)request.getSession().getAttribute("userDTO");
+		
+		if(dto != null) {
+			out.println(dto.getUserId());
+		}
+		
+		
+	}
+
+}
+
